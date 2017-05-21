05a04508e1badecafb9536ac54f48deac59f6643
Christian Frigo <cefrigo@gmail.com>
2017-05-21 01:15:41 -0300

    initial commit

---
+++ Ecommerce121/FlexProductGrid/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Ecommerce121_FlexProductGrid',
+    __DIR__
+);
---
+++ README.md
@@ -0,0 +1,1 @@
+# Flex-Luma-Product-Grid
